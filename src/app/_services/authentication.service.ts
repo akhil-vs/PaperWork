ae234c691e8afa1cf473d2c6d3cf374661e42963
Nikhil V S <vsnikhilvs@gmail.com>
2021-06-20 20:57:00 +0530

    Logout fix

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,4 +1,5 @@
 ﻿import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -11,7 +12,7 @@ export class AuthenticationService {
     private currentUserSubject: BehaviorSubject<User>;
     public currentUser: Observable<User>;
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private router: Router) {
         this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
         this.currentUser = this.currentUserSubject.asObservable();
     }
@@ -34,5 +35,6 @@ export class AuthenticationService {
         // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
         this.currentUserSubject.next(null);
+        this.router.navigateByUrl('login');
     }
 }
